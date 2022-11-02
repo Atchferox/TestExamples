0fc412de00397aefc18eb5a02c22329756dd5ee5
Atchferox <93676830+Atchferox@users.noreply.github.com>
2022-11-02 10:45:56 +0100

    gitignore

--- tests/practise.test.js
+++ tests/practise.test.js
@@ -7,5 +7,7 @@ const testArray = [3, 89, 22, 11]
 
 test('given array returns sum of array', () => {
   // schrijf hier je test
+
+  expect(sumArray(testArray)).toBe(125);
   throw new Error('not implemented yet');
 })
